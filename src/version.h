e374204220f4325d8fbb00d8e09af542f4e25eb6
frank123m <frank123m@gmail.com>
2014-06-01 23:40:17 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -33,7 +33,10 @@ static const int DATABASE_VERSION = 70508;
 static const int PROTOCOL_VERSION = 60020;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 209;
+
+//Hardfork Yay!
+static const int MIN_PROTO_VERSION = 60020;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
