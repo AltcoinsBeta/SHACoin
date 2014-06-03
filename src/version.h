199282caa302c085ecd6a7aef86fb285faa87e2e
f0o <f0o@devilcode.org>
2014-06-03 11:48:41 +0200

    Hardfork yay

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70508;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60020;
+static const int PROTOCOL_VERSION = 60021;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int INIT_PROTO_VERSION = 209;
 
 //Hardfork Yay!
-static const int MIN_PROTO_VERSION = 60020;
+static const int MIN_PROTO_VERSION = 60021;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
