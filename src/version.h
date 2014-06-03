71b2774bd747279e5323edf0d5a074ba458c1d05
frank123m <frank123m@gmail.com>
2014-06-03 18:41:24 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -30,13 +30,13 @@ static const int DATABASE_VERSION = 70508;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60020;
+static const int PROTOCOL_VERSION = 60021;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 60020;
+static const int MIN_PROTO_VERSION = 60021;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
